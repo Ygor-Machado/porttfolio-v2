b152c7c1fc3d37c22b39f81e1671b300a91534d8
Ygao-Underground <ygaounderground011@gmail.com>
2024-12-31 04:43:37 -0300

    feat: Cria migrations e as models

---
+++ app/Models/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'github_link',
+        'deploy_link'
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
